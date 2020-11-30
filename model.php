a9a53f6fb2bc8b84bfa4b5cd4e72dbfc2b40de08
saeedt70 <51361518+saeedt70@users.noreply.github.com>
2020-11-30 13:31:32 +0330

    Add files via upload

---
+++ model.php
@@ -0,0 +1,29 @@
+<?php
+class DB {
+  private $pdo = null;
+  private $stmt = null;
+
+  function __construct(){
+    try {
+      $this->pdo = new PDO("mysql:host=localhost;dbname=student;charset=utf8", "root", "");
+    } 
+catch (Exception $ex) { 
+die($ex->getMessage()); 
+}
+  }
+
+  function exs($sql, $cond=null){
+    $result = false;
+    try {
+      $this->stmt = $this->pdo->prepare($sql);
+      $this->stmt->execute($cond);
+      $result = $this->stmt->fetchAll();
+    }
+ catch (Exception $ex) { 
+die($ex->getMessage()); 
+}
+    $this->stmt = null;
+    return $result;
+  }
+}
+?>
