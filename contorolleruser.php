ebf4232f90d0e957f4f68294ecd0434e15ebe888
saeedt70 <51361518+saeedt70@users.noreply.github.com>
2020-12-01 11:35:48 +0330

    Add files via upload

---
+++ contorolleruser.php
@@ -0,0 +1,9 @@
+<?php
+class Users extends DB {
+  function setstudent( $name,$family,$mobile, $jensiat, $reshte, $addres){
+$sqli="INSERT INTO user (name, family, addres, mobile, jensiat, reshte) VALUES ('".$name."','".$family."','".$mobile."','".$jensiat."','".$reshte."','".$addres."')";
+ return $this->exs($sqli);
+  }
+ 
+}
+?>
