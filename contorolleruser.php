0a3144ba46939a5a11b535bbf0f3503ab31d4fc9
saeedt70 <51361518+saeedt70@users.noreply.github.com>
2020-12-01 11:42:35 +0330

    Update contorolleruser.php

--- contorolleruser.php
+++ contorolleruser.php
@@ -1,4 +1,5 @@
 <?php
+
 class Users extends DB {
   function setstudent( $name,$family,$mobile, $jensiat, $reshte, $addres){
 $sqli="INSERT INTO user (name, family, addres, mobile, jensiat, reshte) VALUES ('".$name."','".$family."','".$mobile."','".$jensiat."','".$reshte."','".$addres."')";
