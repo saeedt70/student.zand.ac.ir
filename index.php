5878ecacd96fcc4a33e3ba87591e4451eeeff311
saeedt70 <51361518+saeedt70@users.noreply.github.com>
2020-12-02 12:35:09 +0330

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+﻿
+<html>
+<body>
+  <center>
+  <table width="400" border="0">
+  
+      <td><a  href="index.php?ro=<?php echo 1; ?>">ثبت نام دانشجو</a></td>
+      <td><a  href="index.php?ro=<?php echo 2; ?>">انتخاب واحد</a></td>
+     
+    </tr>
+  </table>
+  </center>
+</body>
+</html>
+
+ <?php
+if(isset($_GET['ro']))
+{
+	$r=$_GET['ro'];
+	if($r==1)
+  require('viewstudent.php');
+if($r==2)
+  require('viewdars.php');
+
+}
+?>
