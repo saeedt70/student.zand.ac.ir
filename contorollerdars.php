afb48a1f82cc7581ef8893a3410e6303759eb6d9
saeedt70 <51361518+saeedt70@users.noreply.github.com>
2020-12-02 12:33:30 +0330

    Add files via upload

---
+++ contorollerdars.php
@@ -0,0 +1,14 @@
+<?php
+class Users extends DB {
+  function setsdars( $idu,$idd){
+$sqli="INSERT INTO entekhab (idu,idd) VALUES ('".$idu."','".$idd."')";
+ return $this->exs($sqli);
+  }
+
+ function getdars( ){
+$sqli="SELECT * FROM `dars`";
+ return $this->exs($sqli);
+  }
+ 
+}
+?>
